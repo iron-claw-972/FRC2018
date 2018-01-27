3a32099ffb159cc3f8a189f454dacde533380851
Jerry Zhang <jerry.w.zhang@gmail.com>
2018-01-26 18:54:53 -0800

    more code

--- src/org/usfirst/frc/team972/robot/executor/TeleopElevatorTask.java
+++ src/org/usfirst/frc/team972/robot/executor/TeleopElevatorTask.java
@@ -3,12 +3,19 @@ package org.usfirst.frc.team972.robot.executor;
 import org.usfirst.frc.team972.robot.motors.MechanismActuators;
 import org.usfirst.frc.team972.robot.ui.UserInputGamepad;
 
+import edu.wpi.first.wpilibj.Joystick;
+
 public class TeleopElevatorTask extends Task {
 
 	MechanismActuators mechanismMotors;
 	UserInputGamepad uig;
 	
 	double easingValue = 0.25;
+	final int elevatorAxisJoystick = 2;
+	
+	final double deadbandValue = 0.05;
+	
+	double output = 0;
 	
 	public TeleopElevatorTask(double _executionTime, UserInputGamepad _uig, MechanismActuators _mechanismMotors) {
 		super(_executionTime);
@@ -16,6 +23,10 @@ public class TeleopElevatorTask extends Task {
 		uig = _uig;
 	}
 
+    public double handleDeadband(double val, double deadband) {
+        return (Math.abs(val) > Math.abs(deadband)) ? val : 0.0;
+    }
+	
 	private double interpolateValues(double want, double actual) {
 		double error = (want - actual) * easingValue;
 		return actual + error;
@@ -29,7 +40,11 @@ public class TeleopElevatorTask extends Task {
 
 	@Override
 	public void execute(double dt) {
-		
+		Joystick joystickB = uig.getStickB();
+		double wantedOutput = joystickB.getRawAxis(elevatorAxisJoystick);
+		wantedOutput = handleDeadband(wantedOutput, deadbandValue);
+		output = interpolateValues(wantedOutput, output);
+		mechanismMotors.RunElevatorLiftMotor(output);
 	}
 	
 }
